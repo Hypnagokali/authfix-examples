e92914be72a027a600fa3e5347c58bcd7835e9a2
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-04-30 23:02:46 +0200

    [+] Simple session auth

---
+++ simple_session/src/main.rs
@@ -0,0 +1,62 @@
+use actix_web::{get, HttpResponse, HttpServer, Responder};
+use authfix::{async_trait::async_trait, config::Routes, login::{LoadUserByCredentials, LoadUserError, LoginToken}, session::app_builder::SessionLoginAppBuilder, AuthToken};
+use serde::{Deserialize, Serialize};
+
+
+// A user handled by this library needs to implement Clone, Serialize, Deserialize
+#[derive(Clone, Serialize, Deserialize)]
+struct User {
+    name: String
+}
+
+// Struct that handles the authentication
+struct AuthenticationService;
+
+// LoadUsersByCredentials uses async_trait, so its needed when implementing the trait for AuthenticationService
+// async_trait is re-exported by authfix.
+#[async_trait]
+impl LoadUserByCredentials for AuthenticationService {
+    type User = User;
+
+    async fn load_user(&self, login_token: &LoginToken) -> Result<Self::User, LoadUserError> {
+        // load user by email logic and check password
+        // currently authfix does not provide hashing functions, you can use for example https://docs.rs/argon2/latest/argon2/
+        if login_token.email == "test@example.org" && login_token.password == "password" {
+            Ok(User {
+                name: "Johnny".to_owned()
+            })
+        } else {
+            Err(LoadUserError::LoginFailed)
+        }
+    }
+}
+
+// You have access to the user via the AuthToken extractor in secured routes. 
+#[get("/secured")]
+async fn secured(auth_token: AuthToken<User>) -> impl Responder {
+    let user = auth_token.get_authenticated_user().clone();
+    HttpResponse::Ok().json(user)
+}
+
+#[get("/public")]
+async fn public() -> impl Responder {
+    HttpResponse::Ok().json(r#"{ value: "public value" }"#)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        // SessionLoginAppBuilder is the simplest way to create an App instance configured with session based authentication
+        SessionLoginAppBuilder::default(AuthenticationService)
+        // configure path names for the login handler and define paths that are not secured.
+        // Routes::default() registers: /login, /login/mfa, /logout
+        .set_routes_and_unsecured_paths(Routes::default(), vec!["/public"])
+        // create App instance with build()
+        .build()
+        .service(secured)
+        .service(public)
+    })
+    .bind("127.0.0.1:7080")?
+    .run()
+    .await
+}
